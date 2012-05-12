982668e1843df751a53549ed348756fb35ca0772
Kevin <kdeloach@gmail.com>
2012-05-11 21:35:05 -0400

    add origin

--- python/puzzles/triangle.py
+++ python/puzzles/triangle.py
@@ -1,12 +1,17 @@
  
+# Source: http://marketing.yodle.com/downloads/puzzles/triangle.html
+ 
 def linemax(f):
     i = 0
     for line in f:
+        # find the highest value within +/- 1 position
+        # of the max value on the previous line
         lst = [s for s in str(line).strip().split(' ') if s]
-        a = max(i - 1, 0)
-        lst = lst[a:i + 2]
+        ia = max(i - 1, 0)
+        # list of: i-1, i, i+1
+        lst = lst[ia:i+2]
         n = max(lst)
-        i = lst.index(n) + a
+        i = lst.index(n) + ia
         yield int(n)
  
 with open('triangle.txt') as f:
