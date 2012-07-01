a1bbcf09c81cd64b0b0f62e81130e002c6be98ff
Kevin <kdeloach@gmail.com>
2012-07-01 19:37:47 -0400

    added tf2calc

---
+++ python/tf2calc/README.md
@@ -0,0 +1,11 @@
+This tool allows you to find and compare the relative metal values of TF2 items for use in trading.
+
+The prices are based on the standard [TF2 Spreadsheet Prices](http://tf2spreadsheet.blogspot.com/).
+
+Example usage:
+
+  $ python tf2calc.py "strange rocket" "strange submachine" mack
+  Strange Rocket Launcher, 2.55, 1 (dirty)
+  Strange Submachine Gun, 0.66, 1 (dirty)
+  Strange Holy Mackerel, 15.3, 0.11 (dirty)
+  Total: 18.51 refined (7.26 keys)
---
+++ python/tf2calc/tf2calc.py
@@ -0,0 +1,176 @@
+﻿import json
+import re
+import string
+from optparse import OptionParser
+from pprint import pprint
+
+NUM_COLS = 7
+SCRAP_VALUE = 0.11
+
+def col_number(letter):
+    return list('ABCDEFG').index(letter)
+        
+def load_spreadsheet():   
+    def item_row(item):
+        return int(item['title']['$t'].strip(string.letters))
+    def item_col(item):
+        return col_number(item['title']['$t'].strip(string.digits))
+    def item_index(item):
+        return NUM_COLS * item_row(item) + item_col(item)
+    def item_content(item):
+        return item['content']['$t']
+    data = json.loads(open('tf2prices.json').read())
+    spreadsheet = ((item_index(item), item_content(item)) for item in data['feed']['entry'])
+    # Use dict instead of array because there may be skipped rows
+    return dict(spreadsheet)
+    
+def to_rows(spreadsheet):
+    def cell_value(row, col):
+        try:
+            return spreadsheet[NUM_COLS * row + col]
+        except KeyError:
+            return None
+    row = 0
+    while True:
+        row += 1
+        # Kind of lazy...
+        if row >= len(spreadsheet):
+            break
+        A = cell_value(row, col_number('A'))
+        C = cell_value(row, col_number('C'))
+        D = cell_value(row, col_number('D'))
+        E = cell_value(row, col_number('E'))
+        if not A and not C and not D and not E:
+            continue
+        yield (A, C, D, E)
+
+def find(rows, query, exact=False):
+    query = query.lower()
+    def test(col, term):
+        if exact:
+            return col and term == col.lower()    
+        return col and term in col.lower()
+    for row in rows:
+        # All search terms should match at least one column
+        if all(any(test(col, term) for col in row) for term in query.split(' ')):
+            yield row
+        
+def parse_value(value):
+    result = re.match("(\d+(?:\.\d+)?)(?:x)?(?: - ?(\d+(?:\.\d+)?)(?:x)?)?", value)
+    if not result:
+        # If no amount specified, assume at least 1
+        # Example: "scrap", "bud", "refined" 
+        return 1
+    a, b = result.groups()
+    # Take the highest value if described as a range
+    return max(float(a or 0), float(b or 0))
+        
+def parse_unit(value):
+    result = re.search("(keys?|buds?|bills?|ref|refined|wep|weapon|scrap)", value)
+    unit = 'metal'
+    if result:
+        match, = result.groups()
+        unit = match if match else unit
+    return unit
+        
+def make_row_funcs(rows):
+    def find_unit_value(unit, exact=True):
+        if unit not in find_unit_value.cache:
+            find_unit = find(rows, unit, exact=exact)
+            first = next(find_unit)
+            _, _, value, _ = first
+            find_unit_value.cache[unit] = metal_value(value)
+        return find_unit_value.cache[unit]
+    find_unit_value.cache = {}
+    
+    def metal_value(value):
+        amount = parse_value(value)
+        unit = parse_unit(value)
+        if 'key' in unit:
+            return amount * find_unit_value('key')
+        if 'bud' in unit:
+            return amount * find_unit_value('earbuds')
+        if 'bill' in unit:
+            return amount * find_unit_value("bill's hat", exact=False)
+        if 'scrap' in unit:
+            return amount * SCRAP_VALUE
+        if 'wep' in unit or 'weap' in unit:
+            # Defining weapon as half scrap, which is good enough for our purposes
+            return amount * (SCRAP_VALUE / 2)
+        return amount
+    
+    return find_unit_value, metal_value
+
+# A - Quality
+# B - Class
+# C - Item Name
+# D - Value in ref(ined), key(s), bud(s), weap(on)
+# E - Value if non-vintage, non-strange, non-genuine, non-haunted, or dirty
+# F - Notes
+# G - Color
+
+
+
+def run_tests(rows):
+    find_unit_value, metal_value = make_row_funcs(rows)
+    
+    key_metal_value = find_unit_value('key')
+    bud_metal_value = find_unit_value('earbuds')
+    bills_metal_value = find_unit_value("bill's hat", exact=False)
+    
+    assert metal_value('1 key +/-') == key_metal_value
+    assert metal_value('1.33 - 2.33 keys +/-') == key_metal_value * 2.33
+    assert metal_value('1x - 2.66x keys +/-') == key_metal_value * 2.66
+    assert metal_value('24 keys +/-') == key_metal_value * 24
+    assert metal_value('0.66 -') == 0.66
+    assert metal_value('0.66 - 1') == 1
+    assert metal_value('3 ref') == 3
+    assert metal_value('3 refined') == 3
+    assert metal_value('1 bud') == bud_metal_value
+    assert metal_value('2 buds') == bud_metal_value * 2
+    assert metal_value('scrap') == SCRAP_VALUE
+    assert metal_value('2 scraps') == SCRAP_VALUE * 2
+    assert metal_value('bills') == bills_metal_value
+    assert metal_value('2x bills') == bills_metal_value * 2
+    assert metal_value('wep') == SCRAP_VALUE / 2
+    assert metal_value('0.25 wep') == (SCRAP_VALUE / 2) * 0.25
+    assert metal_value('2 weapons') == SCRAP_VALUE
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    opts, args = parser.parse_args()
+    
+    spreadsheet = load_spreadsheet()
+    rows = list(to_rows(spreadsheet))
+    find_unit_value, metal_value = make_row_funcs(rows)
+    
+    run_tests(rows)
+
+    total = 0
+    
+    for query in args:            
+        result = find(rows, query)
+        try:
+            first = next(result)
+            quality, item_name, value, dirty_value = first
+            value = metal_value(value)
+            dirty_value = metal_value(dirty_value)
+            print "%s %s, %s, %s (dirty)" % (quality, item_name, value, dirty_value)
+            total += value
+        except StopIteration:
+            print "%s -- NOT FOUND" % query
+    
+    key_metal_value = find_unit_value('key')
+    total_keys = total / key_metal_value
+    
+    print "Total: {0:.2f} refined ({1:.2f} keys)".format(total, total_keys)
+
+    
+    
+    
+    
+    
+    
+    
+    
+
