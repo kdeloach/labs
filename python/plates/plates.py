9aedd1ef5a66513a8e82fed71971b8569fd436c6
Kevin DeLoach <kdeloach@gmail.com>
2021-09-08 11:28:29 -0400

    Update repmax

--- python/plates/plates.py
+++ python/plates/plates.py
@@ -5,10 +5,10 @@ import sys
 
 # https://symmetricstrength.com/standards#/160/lb/male/31
 lifts = {
-    "Squat": {"repmax": 270},
-    "Bench": {"repmax": 190},
-    "Deadlift": {"repmax": 280},
-    "Press": {"repmax": 130},
+    "Squat": {"repmax": 280},
+    "Bench": {"repmax": 195},
+    "Deadlift": {"repmax": 290},
+    "Press": {"repmax": 135},
 }
 
 
