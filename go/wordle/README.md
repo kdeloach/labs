20230d3d449fc439b7b4bc32e361162ca7290f73
Kevin DeLoach <kdeloach@gmail.com>
2022-01-27 11:36:12 -0500

    Improve wordle solver input format

--- go/wordle/README.md
+++ go/wordle/README.md
@@ -1,94 +1,64 @@
 # wordle solver
 
-First attempt at a Wordle solver.
+Basic utility to filter words to solve the wordle puzzle.
 
 ## Usage
 
 ```sh
 $ go run wordle.go -h
-Usage of /tmp/go-build4148900367/b001/exe/wordle:
-  -allMatch string
-    	Right letters right spot. (ex. "pi--a")
-  -anyMatch string
-    	Right letters wrong spot. Comma delimited list. (ex. "-a---,co---")
-  -best
-    	Find best word from filtered set.
-  -noMatch string
-    	Wrong letters. (ex. "xyz")
+Usage of /tmp/go-build142945034/b001/exe/wordle:
+  -clues string
+    	Comma separated list of clues. (ex. "arose:G-Y--,allot:G--Y-")
 ```
+
+The format for `clues` is a comma separated list of `word:clue` pairs.
+
+`word` is the word you guessed.
+
+`clue` is the result of your guess.
+
+`clue` format is `G-Y--` where `G` means "Right letter right spot", `Y` means
+"Right letter wrong spot", and `-` means "Wrong letter".
 
 ## Demo
 
 Imagine the correct answer is `pizza`.
 
-First, I run the command with blank arguments to see the top 3 best words to
-start with.
+I guess `arose`, `manly`, `whack` before finding the correct answer on the
+fourth try.
 
 ```sh
-$ go run wordle.go -allMatch "" -anyMatch "" -noMatch "" -best | head -n 3
+$ go run wordle.go -clues "" | head -n 3
 arose 0.06%
 stare 0.06%
 raise 0.06%
-```
 
-I input `arose` as my first guess.
-
-The letters `rose` don't match anything so I add those to `noMatch`. This
-filters out all words containing those letters.
-
-The letter `a` matches but it's in the wrong spot so I add `a----` to
-`anyMatch`. This filters out all words that _don't_ contain `a`. Also, it
-filters out words with `a` in that position.
-
-```sh
-$ go run wordle.go -allMatch "" -anyMatch "a----" -noMatch "rose" -best | head -n 3
+$ go run wordle.go -clues "arose:Y----" | head -n 3
 manly 0.86%
 candy 0.84%
 canny 0.83%
-```
 
-Now the best word to guess is `manly`. I input this word as my next guess.
-
-The letters `mnly` don't match anything so I append them to `noMatch` to filter
-out words containing those letters.
-
-The letter `a` matches, but it's in the wrong spot again, so I append `-a---`
-to `anyMatch` to filter out all words with `a` in that position.
-
-```sh
-$ go run wordle.go -allMatch "" -anyMatch "a----,-a---" -noMatch "rosemnly" -best | head -n 3
+$ go run wordle.go -clues "arose:Y----,manly:-Y---" | head -n 3
 whack 18.26%
 khaki 16.52%
 quack 16.52%
-```
 
-I input `whack` as my next guess.
-
-The letters `whck` don't match anything so I append them to `noMatch`.
-
-The letter `a` matches, but it's in the wrong spot again, so I append `--a--`
-to `anyMatch`.
-
-Finally, we see that the correct answer is the top result.
-
-```sh
-$ go run wordle.go -allMatch "" -anyMatch "a----,-a---,--a--" -noMatch "rosemnlywhck" -best | head -n 3
+$ go run wordle.go -clues "arose:Y----,manly:-Y---,whack:--Y--" | head -n 3
 pizza 55.56%
 tibia 44.44%
 ```
 
----
+Imagine the correct answer is `audio`.
 
-Here's another demo where the correct answer is `pizza`.
-
-I guess `strip` and then `pinky` before it returns the only possible solution.
+I guess `arose` and then `allot` before the word list is filtered down to one
+possible result on the third try.
 
 ```sh
-$ go run wordle.go -allMatch "" -anyMatch "---ip" -noMatch "stre" -best | head -n 3
-pinky 10.20%
-picky 9.60%
-hippy 9.60%
+$ go run wordle.go -clues "arose:G-Y--" | head -n 3
+allot 12.76%
+allow 12.76%
+alloy 12.76%
 
-$ go run wordle.go -allMatch "pi---" -anyMatch "---ip" -noMatch "strenky" -best | head -n 3
-pizza
+$ go run wordle.go -clues "arose:G-Y--,allot:G--Y-" | head -n 3
+audio
 ```
--- go/wordle/wordle.go
+++ go/wordle/wordle.go
@@ -5,26 +5,22 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
 	"sort"
 	"strings"
+	"syscall"
 )
 
-var fAllMatch = flag.String("allMatch", "", "Right letters right spot. (ex. \"pi--a\")")
-var fAnyMatch = flag.String("anyMatch", "", "Right letters wrong spot. Comma delimited list. (ex. \"-a---,co---\")")
-var fNoMatch = flag.String("noMatch", "", "Wrong letters. (ex. \"xyz\")")
-var fFindBest = flag.Bool("best", false, "Find best word from filtered set.")
+var fClues = flag.String("clues", "", "Comma separated list of clues. (ex. \"arose:G-Y--,allot:G--Y-\")")
 
 func main() {
+	signal.Ignore(syscall.SIGPIPE)
 	flag.Parse()
 	words := loadWords()
-	words = filterWords(words, *fAllMatch, *fNoMatch, *fAnyMatch)
-	if *fFindBest {
-		bestWord(words)
-	} else {
-		for _, w := range words {
-			fmt.Println(w)
-		}
-	}
+	clues := parseClues(*fClues)
+	filter := createFilterFunc(clues)
+	words = filterWords(words, filter)
+	rankWords(words)
 }
 
 func loadWords() []string {
@@ -48,76 +44,139 @@ func loadWords() []string {
 	return words
 }
 
-func filterWords(words []string, allMatch, noMatch, anyMatchCsv string) []string {
-	skipWord := func(string) bool { return false }
+var (
+	NotSet byte = ' '
+	Green  byte = 'g'
+	Yellow byte = 'y'
+)
 
-	anyMatchSet := []string{}
-	anyMatch := ""
+type Clues struct {
+	allMatch      map[byte]bool
+	allMatchExact []byte
+	noMatch       map[byte]bool
+	noMatchExact  map[byte][]bool
+}
 
-	if len(anyMatchCsv) > 0 {
-		anyMatchSet = strings.Split(anyMatchCsv, ",")
-		for _, c := range anyMatchCsv {
-			if c >= 'a' && c <= 'z' {
-				anyMatch += string(c)
-			}
-		}
+// Parse clues CSV format "word:result,word:result,..."
+func parseClues(cluesCsv string) *Clues {
+	clues := &Clues{
+		allMatch:      map[byte]bool{},
+		allMatchExact: []byte{NotSet, NotSet, NotSet, NotSet, NotSet},
+		noMatch:       map[byte]bool{},
+		noMatchExact:  map[byte][]bool{},
 	}
 
-	if noMatch != "" {
-		oldSkipWord := skipWord
-		skipWord = func(word string) bool {
-			if strings.ContainsAny(word, noMatch) {
+	if len(cluesCsv) == 0 {
+		return clues
+	}
+
+	// parse "word:result,word:result,..."
+	cluesSet := strings.Split(strings.ToLower(cluesCsv), ",")
+
+	for _, wr := range cluesSet {
+		// parse "word:result"
+		pair := strings.Split(wr, ":")
+		if len(pair) != 2 {
+			fmt.Printf("invalid clue format\n")
+			os.Exit(1)
+		}
+
+		w := pair[0]
+		r := pair[1]
+		if len(w) != len(r) {
+			fmt.Printf("invalid clue format: word and result length mismatch\n")
+			os.Exit(1)
+		}
+
+		if len(w) != 5 || len(r) != 5 {
+			fmt.Printf("invalid clue format: word length must be 5 characters\n")
+			os.Exit(1)
+		}
+
+		// parse clue string and compare to guess word
+		// G = right letter right spot (green)
+		// Y = right letter wrong spot (yellow)
+		// everything else = wrong letter
+		for i := 0; i < 5; i++ {
+			c := r[i]
+			if c == Green {
+				// character match (exact position)
+				if clues.allMatchExact[i] == NotSet {
+					clues.allMatchExact[i] = w[i]
+				} else if clues.allMatchExact[i] != w[i] {
+					fmt.Printf(fmt.Sprintf("conflict: multiple exact matches at position %v\n", i+1))
+					os.Exit(1)
+				}
+			} else if c == Yellow {
+				// character match (any position)
+				clues.allMatch[w[i]] = true
+
+				// character no match (exact position)
+				if _, ok := clues.noMatchExact[w[i]]; !ok {
+					clues.noMatchExact[w[i]] = []bool{false, false, false, false, false}
+				}
+				clues.noMatchExact[w[i]][i] = true
+			} else if c == '?' {
+				// unknown character match
+				continue
+			} else if c == '_' || c == '-' || c == ' ' {
+				// characted no match (any position)
+				clues.noMatch[w[i]] = true
+			} else {
+				fmt.Printf(fmt.Sprintf("invalid clue format: unexpected character \"%v\" (must be G, Y, -, _, ?, or space) \n", string(c)))
+				os.Exit(1)
+			}
+		}
+
+	}
+
+	return clues
+}
+
+func isAlpha(c byte) bool {
+	return c >= 'a' && c <= 'z'
+}
+
+type FilterFunc func(word string) bool
+
+func createFilterFunc(clues *Clues) FilterFunc {
+	return func(word string) bool {
+		// handle no match (any)
+		for c, _ := range clues.noMatch {
+			if strings.IndexByte(word, c) != -1 {
 				return true
 			}
-			return oldSkipWord(word)
 		}
-	}
-	if anyMatch != "" {
-		oldSkipWord := skipWord
-		skipWord = func(word string) bool {
-			for _, c := range anyMatch {
-				if strings.Index(word, string(c)) == -1 {
+
+		// handle all match (exact)
+		for i := 0; i < 5; i++ {
+			c := clues.allMatchExact[i]
+			if isAlpha(c) && word[i] != c {
+				return true
+			}
+		}
+
+		// handle all match (any)
+		for c, _ := range clues.allMatch {
+			if strings.IndexByte(word, c) == -1 {
+				return true
+			}
+		}
+
+		// handle no match (exact)
+		for i := 0; i < 5; i++ {
+			if spots, ok := clues.noMatchExact[word[i]]; ok {
+				if spots[i] {
 					return true
 				}
 			}
-			return oldSkipWord(word)
 		}
-	}
-	if len(anyMatchSet) > 0 {
-		oldSkipWord := skipWord
-		skipWord = func(word string) bool {
-			for _, w := range anyMatchSet {
-				for i := 0; i < len(w); i++ {
-					c := w[i]
-					if c >= 'a' && c <= 'z' {
-						if word[i] == c {
-							return true
-						}
-					}
-				}
-			}
-			return oldSkipWord(word)
-		}
-	}
-	if allMatch != "" {
-		oldSkipWord := skipWord
-		skipWord = func(word string) bool {
-			l := len(allMatch)
-			if len(word) < l {
-				l = len(word)
-			}
-			for i := 0; i < l; i++ {
-				c := allMatch[i]
-				if c >= 'a' && c <= 'z' {
-					if word[i] != c {
-						return true
-					}
-				}
-			}
-			return oldSkipWord(word)
-		}
-	}
 
+		return false
+	}
+}
+
+func filterWords(words []string, skipWord FilterFunc) []string {
 	newWords := []string{}
 	for _, w := range words {
 		if skipWord(w) {
@@ -134,7 +193,7 @@ type record struct {
 	rank float64
 }
 
-func bestWord(words []string) {
+func rankWords(words []string) {
 	records := make([]*record, len(words))
 
 	for i, w := range words {
